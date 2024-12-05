cc27f392d9554364ade2033557cd8dfd5f0a161a
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-12-05 12:11:25 +1100

    Configure PWA

--- mousike/src/main/java/com/thomasvitale/mousike/MousikeApplication.java
+++ mousike/src/main/java/com/thomasvitale/mousike/MousikeApplication.java
@@ -1,6 +1,7 @@
 package com.thomasvitale.mousike;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 
 import org.springframework.boot.SpringApplication;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Theme(value = "mousike")
+@PWA(name = "Mousike", shortName = "Mousike", iconPath = "icons/icon.png")
 public class MousikeApplication implements AppShellConfigurator {
 
 	public static void main(String[] args) {
